ea6e60e8d60634d7c3f4943864a95cfa8fb8e1b0
sparsick <sparsick@web.de>
2017-04-07 15:46:00 +0200

    #13: add tests to demonstrate the issue

--- src/test/java/guru/nidi/graphviz/engine/GraphvizTest.java
+++ src/test/java/guru/nidi/graphviz/engine/GraphvizTest.java
@@ -14,10 +14,32 @@ import static org.junit.Assert.*;
 public class GraphvizTest {
 
     @Test
-    public void methodChainCheck(){
+    public void scaleMethodChainCheck(){
         final Graph graph = graph().with(node("a").link("b"));
 
         final Graphviz graphviz = Graphviz.fromGraph(graph).height(20).width(30).scale(3);
+
+        assertThat(graphviz.width, is(30) );
+        assertThat(graphviz.height, is(20) );
+        assertThat(graphviz.scale, is(3d ));
+    }
+
+    @Test
+    public void heightMethodChainCheck(){
+        final Graph graph = graph().with(node("a").link("b"));
+
+        final Graphviz graphviz = Graphviz.fromGraph(graph).scale(3).width(30).height(20);
+
+        assertThat(graphviz.width, is(30) );
+        assertThat(graphviz.height, is(20) );
+        assertThat(graphviz.scale, is(3d ));
+    }
+
+    @Test
+    public void widthMethodChainCheck(){
+        final Graph graph = graph().with(node("a").link("b"));
+
+        final Graphviz graphviz = Graphviz.fromGraph(graph).scale(3).height(20).width(30);
 
         assertThat(graphviz.width, is(30) );
         assertThat(graphviz.height, is(20) );
