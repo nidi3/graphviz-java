c6205c5b83e6e50d2355bfa4fcb82c64cb2d6638
sparsick <sparsick@web.de>
2017-04-08 08:57:23 +0200

    #13: fix code analyse issues

--- src/test/java/guru/nidi/graphviz/engine/GraphvizTest.java
+++ src/test/java/guru/nidi/graphviz/engine/GraphvizTest.java
@@ -1,13 +1,27 @@
+/*
+ * Copyright (C) 2015 Stefan Niederhauser (nidin@gmx.ch)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package guru.nidi.graphviz.engine;
-
-import guru.nidi.graphviz.model.Graph;
-import org.junit.Test;
 
 import static guru.nidi.graphviz.model.Factory.graph;
 import static guru.nidi.graphviz.model.Factory.node;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
+import guru.nidi.graphviz.model.Graph;
+import org.junit.Test;
 
 public class GraphvizTest {
 
@@ -17,9 +31,7 @@ public class GraphvizTest {
 
         final Graphviz graphviz = Graphviz.fromGraph(graph).height(20).width(30).scale(3);
 
-        assertThat(graphviz.width, is(30) );
-        assertThat(graphviz.height, is(20) );
-        assertThat(graphviz.scale, is(3d ));
+        assertThatGraphvizHasFields(graphviz, 20, 30, 3d);
     }
 
     @Test
@@ -28,9 +40,7 @@ public class GraphvizTest {
 
         final Graphviz graphviz = Graphviz.fromGraph(graph).scale(3).width(30).height(20);
 
-        assertThat(graphviz.width, is(30) );
-        assertThat(graphviz.height, is(20) );
-        assertThat(graphviz.scale, is(3d ));
+        assertThatGraphvizHasFields(graphviz, 20, 30, 3d);
     }
 
     @Test
@@ -39,8 +49,12 @@ public class GraphvizTest {
 
         final Graphviz graphviz = Graphviz.fromGraph(graph).scale(3).height(20).width(30);
 
-        assertThat(graphviz.width, is(30) );
-        assertThat(graphviz.height, is(20) );
-        assertThat(graphviz.scale, is(3d ));
+        assertThatGraphvizHasFields(graphviz, 20, 30, 3d);
+    }
+
+    private void assertThatGraphvizHasFields(Graphviz graphviz, int expectedHeight, int expectedWidth, double expectedScale ) {
+        assertThat(graphviz.width, is(expectedWidth) );
+        assertThat(graphviz.height, is(expectedHeight) );
+        assertThat(graphviz.scale, is(expectedScale ));
     }
 }
