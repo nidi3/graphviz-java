2b1666a4984994e69aa90c5872f3c5f34710a70c
Stefan Niederhauser <ghuder5@gmx.ch>
2018-08-09 01:38:59 +0200

    first version of kotlin module

---
+++ graphviz-kotlin/src/test/kotlin/guru/nidi/graphviz/CodeAnalysisTest.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright © 2015 Stefan Niederhauser (nidin@gmx.ch)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package guru.nidi.graphviz
+
+import guru.nidi.codeassert.checkstyle.CheckstyleAnalyzer
+import guru.nidi.codeassert.checkstyle.StyleEventCollector
+import guru.nidi.codeassert.config.AnalyzerConfig
+import guru.nidi.codeassert.config.CollectorConfig.just
+import guru.nidi.codeassert.config.In
+import guru.nidi.codeassert.config.Language.KOTLIN
+import guru.nidi.codeassert.dependency.*
+import guru.nidi.codeassert.findbugs.BugCollector
+import guru.nidi.codeassert.findbugs.FindBugsAnalyzer
+import guru.nidi.codeassert.junit.CodeAssertJunit5Test
+import guru.nidi.codeassert.junit.PredefConfig
+import guru.nidi.codeassert.junit.kotlin.KotlinCodeAssertMatchers.hasNoKtlintIssues
+import guru.nidi.codeassert.ktlint.KtlintAnalyzer
+import guru.nidi.codeassert.ktlint.KtlintCollector
+import guru.nidi.codeassert.pmd.*
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Test
+
+class CodeAnalysisTest : CodeAssertJunit5Test() {
+    private val config = AnalyzerConfig.maven(KOTLIN).main()!!
+
+    override fun analyzeDependencies(): DependencyResult {
+        return DependencyAnalyzer(config)
+                .rules(DependencyRules.denyAll()
+                        .withExternals("java.*", "kotlin.*", "org.*")
+                        .withRelativeRules(object : DependencyRuler() {
+                            val attribute = rule()
+                            val model = rule()
+
+                            override fun defineRules() {
+                                model.mayUse(attribute)
+                            }
+                        }))
+                .analyze()
+    }
+
+    override fun analyzeCheckstyle() = CheckstyleAnalyzer(config, PredefConfig.adjustedGoogleStyleChecks(), StyleEventCollector()
+            .apply(PredefConfig.minimalCheckstyleIgnore()))
+            .analyze()!!
+
+    override fun analyzeCpd() = CpdAnalyzer(config, 25, CpdMatchCollector()).analyze()!!
+
+    override fun analyzePmd() = PmdAnalyzer(config, PmdViolationCollector()
+            .apply(PredefConfig.minimalPmdIgnore()))
+            .withRulesets(*PredefConfig.defaultPmdRulesets())
+            .analyze()!!
+
+    override fun analyzeFindBugs() = FindBugsAnalyzer(config, BugCollector()
+            .apply(PredefConfig.minimalFindBugsIgnore())
+            .config(just(In.classes("Kraphviz").withMethods("graph").ignore("BC_UNCONFIRMED_CAST_OF_RETURN_VALUE"))))
+            .analyze()!!
+
+    @Test
+    fun ktlint() {
+        val result = KtlintAnalyzer(config, KtlintCollector()
+                .because("I think it's ok", In.everywhere().ignore("no-wildcard-imports")))
+                .analyze()
+        assertThat(result, hasNoKtlintIssues())
+    }
+}
---
+++ graphviz-kotlin/src/test/kotlin/guru/nidi/graphviz/CodeCoverage.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2015 Stefan Niederhauser (nidin@gmx.ch)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package guru.nidi.graphviz
+
+import guru.nidi.codeassert.config.For
+import guru.nidi.codeassert.jacoco.CoverageCollector
+import guru.nidi.codeassert.jacoco.CoverageType.*
+import guru.nidi.codeassert.jacoco.JacocoAnalyzer
+import guru.nidi.codeassert.junit.CodeAssertMatchers.hasEnoughCoverage
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+class CodeCoverage {
+    @Test
+    @Disabled
+    fun coverage() {
+        //TODO 75,75,75 should be the goal
+        val analyzer = JacocoAnalyzer(CoverageCollector(BRANCH, LINE, METHOD)
+                .just(For.global().setMinima(35, 70, 70))
+                .just(For.allPackages().setMinima(50, 50, 50))
+        )
+        assertThat(analyzer.analyze(), hasEnoughCoverage())
+    }
+}
