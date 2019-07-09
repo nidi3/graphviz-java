3b6ebaa8cc18b017c2267d7fefd873b7971f6ba6
Stefan Niederhauser <ghuder5@gmx.ch>
2019-07-09 05:52:05 +0200

    try fix images for windows
    Signed-off-by: Stefan Niederhauser <ghuder5@gmx.ch>

--- graphviz-java/src/main/java/guru/nidi/graphviz/engine/Options.java
+++ graphviz-java/src/main/java/guru/nidi/graphviz/engine/Options.java
@@ -152,7 +152,7 @@ public final class Options {
         final String eng = ",engine:'" + (raw ? engine : engine.toString().toLowerCase(ENGLISH)) + "'";
         final String mem = totalMemory == null ? "" : (",totalMemory:'" + totalMemory + "'");
         final String yInv = yInvert == null ? "" : (",yInvert:" + yInvert);
-        final String base = ",basedir:'" + basedir.getAbsolutePath() + "'";
+        final String base = ",basedir:'" + SystemUtils.uriPathOf(basedir.getAbsolutePath()) + "'";
         final String imgs = ",images:[" + images.stream().map(Image::toJson).collect(joining(",")) + "]";
         return "{" + form + eng + mem + yInv + base + imgs + "}";
     }
