71c2894d97f3d76c5eccfe438aaf4657adfb0a3b
Stefan Niederhauser <ghuder5@gmx.ch>
2020-05-14 23:14:42 +0200

    improve ImageTest
    Signed-off-by: Stefan Niederhauser <ghuder5@gmx.ch>

--- graphviz-java/src/test/java/guru/nidi/graphviz/engine/ImageTest.java
+++ graphviz-java/src/test/java/guru/nidi/graphviz/engine/ImageTest.java
@@ -22,8 +22,10 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.IOException;
 
+import static guru.nidi.graphviz.engine.Format.PNG;
 import static guru.nidi.graphviz.model.Factory.graph;
 import static guru.nidi.graphviz.model.Factory.node;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ImageTest {
     @BeforeAll
@@ -33,29 +35,35 @@ class ImageTest {
 
     @Test
     void relative() throws IOException {
-        Graphviz g = Graphviz.fromGraph(graph()
-                .with(node(" ").with(Image.of("example/graphviz.png"))));
-        g.render(Format.PNG).toFile(new File("target/img-relative.png"));
+        final Graphviz g = Graphviz.fromGraph(graph().with(node(" ").with(Image.of("example/graphviz.png"))));
+        final File out = new File("target/img-relative.png");
+        g.render(PNG).toFile(out);
+        assertTrue(out.length() > 15000);
     }
 
     @Test
     void relativeWithBase() throws IOException {
-        Graphviz g = Graphviz.fromGraph(graph()
-                .with(node(" ").with(Image.of("graphviz.png"))));
-        g.basedir(new File("example")).render(Format.PNG).toFile(new File("target/img-base-relative.png"));
+        final Graphviz g = Graphviz.fromGraph(graph().with(node(" ").with(Image.of("graphviz.png"))));
+        final File out = new File("target/img-base-relative.png");
+        g.basedir(new File("example")).render(PNG).toFile(out);
+        assertTrue(out.length() > 15000);
     }
 
     @Test
     void absolute() throws IOException {
-        Graphviz g = Graphviz.fromGraph(graph()
-                .with(node(" ").with(Image.of(new File("example/graphviz.png").getAbsolutePath()))));
-        g.render(Format.PNG).toFile(new File("target/img-absolute.png"));
+        final Graphviz g = Graphviz.fromGraph(graph().with(node(" ").with(Image.of(
+                new File("example/graphviz.png").getAbsolutePath()))));
+        final File out = new File("target/img-absolute.png");
+        g.render(PNG).toFile(out);
+        assertTrue(out.length() > 15000);
     }
 
     @Test
     void http() throws IOException {
-        Graphviz g = Graphviz.fromGraph(graph()
-                .with(node(" ").with(Image.of("https://raw.githubusercontent.com/nidi3/graphviz-java/master/graphviz-java/example/ex7.png"))));
-        g.render(Format.PNG).toFile(new File("target/img-http.png"));
+        final Graphviz g = Graphviz.fromGraph(graph().with(node(" ").with(Image.of(
+                "https://raw.githubusercontent.com/nidi3/graphviz-java/master/graphviz-java/example/ex7.png"))));
+        final File out = new File("target/img-http.png");
+        g.render(PNG).toFile(out);
+        assertTrue(out.length() > 15000);
     }
 }
