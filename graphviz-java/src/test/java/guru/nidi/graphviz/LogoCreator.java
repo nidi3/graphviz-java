852dddbdc763fd9fe5aef55e726a508b728d3684
Stefan Niederhauser <ghuder5@gmx.ch>
2021-01-27 17:06:26 +0100

    add logo
    Signed-off-by: Stefan Niederhauser <ghuder5@gmx.ch>

---
+++ graphviz-java/src/test/java/guru/nidi/graphviz/LogoCreator.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright © 2015 Stefan Niederhauser (nidin@gmx.ch)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package guru.nidi.graphviz;
+
+import guru.nidi.graphviz.attribute.*;
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
+import guru.nidi.graphviz.model.Graph;
+import guru.nidi.graphviz.model.Node;
+
+import java.io.File;
+import java.io.IOException;
+
+import static guru.nidi.graphviz.attribute.Color.GREY80;
+import static guru.nidi.graphviz.attribute.Color.WHITE;
+import static guru.nidi.graphviz.attribute.GraphAttr.pad;
+import static guru.nidi.graphviz.attribute.Rank.RankDir.LEFT_TO_RIGHT;
+import static guru.nidi.graphviz.model.Factory.graph;
+import static guru.nidi.graphviz.model.Factory.node;
+
+class LogoCreator {
+    public static void main(String[] args) throws IOException {
+        final Image duke = Image.of("graphviz-java/src/test/resources/duke.png");
+        final Size size = Size.mode(Size.Mode.FIXED).size(30, 20);
+        final Node d = node("b").with(Label.of(""), duke, size);
+        final Node c = node("\n\n\nJava").with(duke, size).link(d);
+        final Node b = node("\n\n\nGraphviz").with(duke, size).link(d);
+        final Node a = node("a").with(Label.of(""), duke, size).link(b, c);
+        final Style<ForAll> lineWidth = Style.lineWidth(10);
+        final Graph g = graph()
+                .graphAttr().with(GREY80.gradient(WHITE).background().angle(90), Rank.dir(LEFT_TO_RIGHT), pad(5, 5))
+                .linkAttr().with(lineWidth)
+                .nodeAttr().with(lineWidth, Style.FILLED, Color.WHITE.fill(), Font.size(140))
+                .with(a);
+        Graphviz.fromGraph(g).width(1280).height(640).render(Format.PNG).toFile(new File("logo"));
+    }
+}
