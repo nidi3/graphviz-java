0e21983b58979bbdf66efb7b07569647db72c367
syoon2 <syoon2@mta.ca>
2023-05-25 13:15:46 +0900

    Javadoc for Graph interface

--- graphviz-java/src/main/java/guru/nidi/graphviz/model/Graph.java
+++ graphviz-java/src/main/java/guru/nidi/graphviz/model/Graph.java
@@ -19,26 +19,102 @@ import guru.nidi.graphviz.attribute.*;
 
 import java.util.List;
 
+/**
+ * A Graphviz graph.
+ * 
+ * @see <a href="https://graphviz.org/docs/graph/">Graph</a>
+ */
 public interface Graph extends LinkSource, LinkTarget {
+    /**
+     * Makes a copy of this graph as a strict graph.
+     * 
+     * @return a copy of this graph, as a strict graph.
+     * 
+     * @see <a href=
+     *      "https://graphviz.org/doc/info/lang.html#lexical-and-semantic-notes">Lexical
+     *      and Semantic Notes</a>
+     */
     Graph strict();
 
+    /**
+     * Makes a copy of this graph as a directed graph.
+     * 
+     * @return a copy of this graph, as a directed graph.
+     * 
+     * @see <a href=
+     *      "https://graphviz.org/doc/info/lang.html#lexical-and-semantic-notes">Lexical
+     *      and Semantic Notes</a>
+     */
     Graph directed();
 
+    /**
+     * Makes a copy of this graph as a cluster.
+     * 
+     * @return a copy of this graph, as a cluster
+     * 
+     * @see <a href=
+     *      "https://graphviz.org/doc/info/lang.html#subgraphs-and-clusters">Subgraphs
+     *      and Clusters</a>
+     */
     Graph cluster();
 
+    /**
+     * Makes a copy of this graph with the specified name.
+     * 
+     * @param name a name for the copied graph
+     * @return a copy of this graph with the specified name
+     */
     Graph named(String name);
 
+    /**
+     * Creates links to the specified elements
+     * 
+     * @param targets elements to link this graph
+     * @return a graph
+     */
     Graph link(LinkTarget... targets);
 
+    /**
+     * Creates a graph with the specified elements.
+     * 
+     * @param sources elements to add to the graph
+     * @return a graph with the specified elements
+     */
     Graph with(LinkSource... sources);
 
+    /**
+     * Creates a graph with the specified elements.
+     * 
+     * @param sources list of elements to add to the graph
+     * @return a graph with the specified elements
+     */
     Graph with(List<? extends LinkSource> sources);
 
+    /**
+     * Sets global node attributes.
+     * 
+     * @return node attributes
+     */
     Attributed<Graph, ForNode> nodeAttr();
 
+    /**
+     * Sets global link attributes.
+     * 
+     * @return link attributes
+     */
     Attributed<Graph, ForLink> linkAttr();
 
+    /**
+     * Sets global link attributes.
+     * 
+     * @return link attributes
+     */
     Attributed<Graph, ForGraph> graphAttr();
 
+    /**
+     * Returns this graph as a mutable graph.
+     * 
+     * @return a mutable graph
+     */
     MutableGraph toMutable();
 }
